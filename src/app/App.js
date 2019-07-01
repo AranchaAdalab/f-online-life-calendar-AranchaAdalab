63df69184351f4a4893bb8daee213ba4249afd42
ARANCHA NARVÁEZ ALONSO <aranzazunarvaez@gmail.com>
2019-07-01 20:11:19 +0200

    Write message if happy

--- src/app/App.js
+++ src/app/App.js
@@ -5,12 +5,28 @@ import Editor from "../editor/Editor";
 import { Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mood: ''
+    }
+    this.getHappy = this.getHappy.bind(this);
+  }
+
+  getHappy(event) {
+    const selectValue = event.currentTarget.value;
+    console.log(selectValue);
+    this.setState ({
+      mood: selectValue
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <Switch>
           <Route exact path="/" render={() => <Home />} />
-          <Route path="/editor" render={() => <Editor />} />
+          <Route path="/editor" render={() => <Editor getHappy={this.getHappy} getMood={this.state.mood}/>} />
         </Switch>
       </div>
     );
--- src/editor/Editor.js
+++ src/editor/Editor.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 class Editor extends React.Component {
   render() {
+    const { getHappy, getMood } = this.props;
     return (
       <div className="editor__container">
         <form action="/signup" method="post">
@@ -14,15 +15,27 @@ class Editor extends React.Component {
           <div className="state__container">
             <h2>Estado</h2>
             <label htmlFor="happy">
-              <input id="happy" type="radio" value="happy" name="state" />
+              <input
+                id="happy"
+                type="radio"
+                value="happy"
+                name="state"
+                onChange={getHappy}
+              />
               :)
             </label>
             <label htmlFor="sad">
-              <input id="sad" type="radio" value="sad" name="state" />
+              <input
+                id="sad"
+                type="radio"
+                value="sad"
+                name="state"
+                onChange={getHappy}
+              />
               :(
             </label>
           </div>
-          <div className="reason__container">
+          <div className={`reason__container ${getMood === 'sad' || getMood === '' ? 'hidden' : ''}`}>
             <label htmlFor="reason">Mensaje</label>
             <input
               placeholder="¿Por qué es un buen día"
@@ -31,10 +44,10 @@ class Editor extends React.Component {
               name="reason"
             />
           </div>
-          <Link to={'/'}>
+          <Link to={"/"}>
             <input type="submit" value="Guardar" />
           </Link>
-          <Link to={'/'}>
+          <Link to={"/"}>
             <button>Cancelar</button>
           </Link>
         </form>
